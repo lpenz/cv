1d46de7fc17ab80a5317611e68a325a7bfca7418
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-06-11 21:47:37 +0100

    Add default.nix for nix build

---
+++ default.nix
@@ -0,0 +1,37 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "lpenz-cv";
+
+  src = ./.;
+
+  builder = builtins.toFile "builder.sh" "
+#!/bin/bash -e
+
+source $stdenv/setup
+
+cp -Rs $src/* .
+chmod -R u+w .
+ninja
+mkdir $out
+cp -R * $out/
+";
+
+  buildInputs = [
+    coreutils
+    ninja
+    pandoc
+    (python27.buildEnv.override {
+        extraLibs = with python27Packages; [
+            jinja2
+            pyyaml
+        ];
+    })
+    (texlive.combine {
+        inherit (texlive)
+        scheme-small
+        moderncv
+        ;
+    })
+  ];
+}
